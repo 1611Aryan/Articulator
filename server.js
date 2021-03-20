bc52adae7132cc3b5e6c61952b6bd8202582ea61
Aryan <aryan2563737@gmail.com>
2021-03-21 03:27:25 +0530

    ReBuild

--- server.js
+++ server.js
@@ -3,12 +3,11 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const cors = require("cors");
-if (process.env.NODE_ENV !== "production")
-    require("dotenv").config();
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
 const app = express();
 //Middleware
 const corsOptions = {
-    exposedHeaders: "authToken",
+  exposedHeaders: "authToken",
 };
 app.use(cors(corsOptions));
 app.use(express.json());
@@ -25,8 +24,8 @@ app.use("/user", authenticationRouter);
 app.use("/notebook", notebookRouter);
 //Static Serve
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "client/build")));
-    app.use("/signup", express.static(path.join(__dirname, "client/build")));
-    app.use("/dashboard", express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "Client")));
+  app.use("/signup", express.static(path.join(__dirname, "Client")));
+  app.use("/dashboard", express.static(path.join(__dirname, "Client")));
 }
 app.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
